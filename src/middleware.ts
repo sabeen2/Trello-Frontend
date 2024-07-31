c7387132c7ea721a8b2f991bf425d1b867997fba
Sabin Pandey <sabeenpandey2@gmail.com>
2024-07-31 12:21:56 +0545

    asa

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,16 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
+// export function middleware(request: NextRequest) {
+//   const { pathname } = request.nextUrl;
 
-  // Static path checks to restrict `/` access
-  if (pathname === "/") {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+//   if (pathname === "/") {
+//     return NextResponse.redirect(new URL("/login", request.url));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: ["/", "/dashboard", "/login", "/signup"],
-};
+// export const config = {
+//   matcher: ["/", "/dashboard", "/login", "/signup"],
+// };
